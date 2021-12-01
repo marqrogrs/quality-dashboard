2569c0153d5cc022c958a680c92a8a724386e865
Mark Rogers <mark@birdhaven.us>
2021-12-01 14:48:24 -0500

    Configure github workflow for production

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders coming soon text', () => {
+  render(<App />)
+  const h1Element = screen.getByText(/Coming Soon/i)
+  expect(h1Element).toBeInTheDocument()
+})
